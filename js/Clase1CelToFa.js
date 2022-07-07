0f4d7e8bd030ee8f511b6cc07515b519b852656f
Jiflon <laeisraelflores@hotmail.com>
2022-07-07 12:51:01 -0500

    Clase2

---
+++ js/Clase1CelToFa.js
@@ -0,0 +1,6 @@
+let celsius = prompt('Ingresa la temperatura de tu ciudad: ', 23);
+
+let fahrenheit = parseInt(celsius) * 9/5 + 32;
+
+console.log(`La temperatura en grados fahrenheit:  ${fahrenheit}`);
+
