0ae7a6c12758ab61448627fe3c7b44336fcd88ad
Jiflon <laeisraelflores@hotmail.com>
2022-07-07 22:40:03 -0500

    Clase3 Metodos

---
+++ clase3Methods.js
@@ -0,0 +1,48 @@
+// !Metodos de strings 
+
+// Los 'fer' 'f','e','r'
+0, 1, 2          
+
+// length  -> Propiedad 
+    // Nos devuelve la longitud de un string 
+
+// Metodos: (Ninguno de los metodos de string modifica el string original, en caso de requerirlo tenemos que hacerlo permancer en alguna variable)
+
+//  chartAt(<index al cual acceder>)
+    // Devuleve el caracter del index indicado 
+
+//  toUpperCase()
+    // Nos retorna un string en mayuscula 
+
+// toLowerCase()
+    // Nos retorna un string en minuscula 
+
+// subString(<posicion incial> , ?<posicion final> )
+    // Retorna la porcion de string indicada 
+
+// slice(<posicion incial> , ?<posicion final>)
+    // Retorna la porcion de string indicada
+    // Acepta nuemro negativos (los lee del final hasta el inicio)
+
+// indexOf(<valor a buscar>, <?Poscion incial>)
+    // Regresa el indice de la primera conindicendia del caracter indicado 
+    // En caso que no lo encuentre regresa -1
+
+// Replace(<valor a buscar>, <valor de cambio>)
+    // Cambia la primera coincidencia del valor indicado
+
+// replaceAll(<valor a buscar>, <valor de cambio>) 
+    // Cambia todas las coincidencias del valor indicado
+
+// Trim()
+    // Limpia el string de espacio al final y el inicio
+
+let koder = 'hola koder'
+// let vocales = koder.match(/[aeiou]/gi)
+ 
+
+console.log(koder.search(''))
+console.log(vocales.length)
+
+
+['a']
